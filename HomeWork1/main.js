9fad3a65dfe8bc7a1a31e5286ea431469d1371b3
rus <embawood.kh@gmail.com>
2015-09-16 18:17:03 +0400

    first home work

---
+++ HomeWork1/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rus on 16.09.2015.
+ */
+ var   age = 27;
+ var ageMax = 63;
+ var  fish = 2;
+ var f = 'fishmac';
+ var total = (ageMax - age)*fish*365;
+ alert('вы должны купить '+total+' '+f);
+////////////////////////////////////////////////////////
+
+
+var f = 200;
+var h = (f-32)*5/9;
+console.log(f + "°F is " + h + "°C");
+
+var c = 24;
+var f = c*9/5+32;
+console.log(f + "°F is "+ c + "°C");
